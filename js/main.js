130f9d8f7cc685777f79f32bf4b1bd23f23bf24f
Rodolfo Oliveira <tiranorod@gmail.com>
2019-08-31 17:07:19 -0300

    Created the mobile and widescreen styles and corrected the sticky menu background fading so that it would become solid again when I scrolled back up

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 //Initialize and add the map
 function initMap() {
   // Your Location stored in a const binding
-  const loc = {lat: 46.1066, lng: -64.8296};
+  const loc = { lat: 46.1066, lng: -64.8296 };
   
   //Center map on location chosen
-  const map = new google.maps.Map(document.querySelector('.map')
+  const map = new google.maps.Map(document.querySelector( '.map' )
   , {
     center: loc,
     zoom: 14
@@ -14,16 +14,25 @@ function initMap() {
   const marker = new google.maps.Marker( { position: loc, map: map } );
 }
 
+//Sticky Menu Background Fading
+window.addEventListener( 'scroll', function() {
+  if ( window.scrollY > 150 ) {
+    document.querySelector( '#navbar' ).style.opacity = 0.9;
+  } else {
+    document.querySelector( '#navbar' ).style.opacity = 1;
+  }
+});
+
 //Smooth Scrolling using jQuery
-$('#navbar a, .btn').on('click', function(event){
+$( '#navbar a, .btn' ).on( 'click', function(event){
   if (this.hash !== '') {
     event.preventDefault();
 
     const hash = this.hash;
 
-    $('html, body').animate(
+    $( 'html, body' ).animate(
       {
-        scrollTop: $(hash).offset().top - 82
+        scrollTop: $( hash ).offset().top - 82
       },
       1000
     );
