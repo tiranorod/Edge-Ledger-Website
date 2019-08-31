0525bb44abda062827fd4a00884d4853d1878d30
Rodolfo Oliveira <tiranorod@gmail.com>
2019-08-31 19:11:05 -0300

    finished editing the mobile styles

--- js/main.js
+++ js/main.js
@@ -25,14 +25,25 @@ window.addEventListener( 'scroll', function() {
 
 //Smooth Scrolling using jQuery
 $( '#navbar a, .btn' ).on( 'click', function(event){
-  if (this.hash !== '') {
+  if (this.hash !== '' && window.innerWidth > 500) {
     event.preventDefault();
 
     const hash = this.hash;
 
     $( 'html, body' ).animate(
       {
-        scrollTop: $( hash ).offset().top - 82
+        scrollTop: $( hash ).offset().top - 65
+      },
+      1000
+    );
+  } else {                             //Because the navbar grows to 119 pixels of height 
+    event.preventDefault();            //when the innerWidth of window is less than 500px
+                                       //I've added this else that will make the Smooth
+    const hash = this.hash;            //Scrolling feature also responsive (the navbar will
+                                       //always scroll up to exacty the top of the Session
+    $( 'html, body' ).animate(         //no matter if it is on a desktop or mobile).
+      {
+        scrollTop: $( hash ).offset().top - 119
       },
       1000
     );
